4ca06c9fa73a635a944a91cd5e57172a16e1b81a
circuspony <endofrever@gmail.com>
2021-03-01 22:44:50 +0700

    ee

--- server.pl
+++ server.pl
@@ -293,7 +293,7 @@ create_playlist(Request):-
    assert(playlist_exists(NewID,Author, Name,Description,Image)),
    split_string(Array, ',', ',', NewArray),
    assert_arrays(NewArray,NewID),
-    tell('playlists_data.pl'),   
+    tell('playlists_daa.pl'),   
     listing(playlist_exists),
     listing(playlist_has_song),
     told,
